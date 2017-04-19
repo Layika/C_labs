fb6c1f7a959776f8a97c1b3ce62b28a4271f9ac5
Michalina Oleksy <layika@protonmail.com>
2017-04-19 21:47:06 +0200

    Added qsort_bsearch.c

---
+++ sem2/lab7/qsort_bsearch.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#define EPSILON 0.00001
+#define STRING_LENGTH 20
+
+int  cmp_double(const void* a, const void* b) {
+    if (*(const double*)a > *(const double*)b)
+        return 1;
+    if (*(const double*)a < *(const double*)b)
+        return -1;
+    else return 0;
+}
+
+int cmp_strings(const void* a, const void* b) {
+    return strcmp((const char*)a, *(const char**)b);
+}
+
+void print_double_arr(double* arr, size_t size) {
+    printf("[ ");
+    for (size_t i = 0; i < size; ++i)
+        printf("%lf ", arr[i]);
+    puts(" ]");
+}
+
+void print_strings_arr(char** arr, size_t size) {
+    printf("[ ");
+    for (size_t i = 0; i < size; ++i)
+        printf("%s ", arr[i]);
+    puts(" ]");
+}
+
+void sort_double_arr() {
+    double numbers[] = { 7.4, 1.3, 14.5, 0.1, -1.0, 2.3, 1.2, 43.0, 2.0, -4.7, 5.8, 0 };
+    size_t len = sizeof(numbers) / sizeof(numbers[0]);
+
+    puts("Before sorting double:");
+    print_double_arr(numbers, len);
+
+    puts("After sorting double:");
+    qsort(numbers, len, sizeof(numbers[0]), cmp_double);
+    print_double_arr(numbers, len);
+
+    puts("Provide a number to find in numbers array:");
+    double check;
+    scanf("%lf", &check);
+    if (bsearch(&check, numbers, len, sizeof(numbers[0]), cmp_double))
+        printf("Numbers array contains %lf.\n", check);
+}
+
+void sort_strings_arr() {
+    char* strings[] = { "Zorro", "Alex", "Celine", "Bill", "Forest", "Dexter" };
+    size_t len = sizeof(strings) / sizeof(strings[0]);
+
+    puts("Before sorting strings:");
+    print_strings_arr(strings, len);
+
+    puts("After sorting strings:");
+    qsort(strings, len, sizeof(strings[0]), cmp_strings);
+    print_strings_arr(strings, len);
+
+    puts("Provide a string to find in array:");
+    char check[STRING_LENGTH];
+    scanf("%20s", check);
+    if (bsearch(check, strings, len, sizeof(strings[0]), cmp_strings))
+        printf("Numbers array contains %s.\n", check);
+}
+
+int main() {
+    sort_double_arr();
+    sort_strings_arr();
+
+    return 0;
+}
