ad40f66510ec42bfef3128fa1835582c32d9ef26
Michalina Oleksy <layika@protonmail.com>
2017-04-21 10:03:52 +0200

    Update format.c

--- sem2/lab6/format.c
+++ sem2/lab6/format.c
@@ -7,11 +7,11 @@
 #endif
 
 int main(){
-		char pi_fmt[6] = {0};
-		for (int i = 0; i < 7; i += 2){
-				sprintf(pi_fmt, "%d", i);
-				printf("FORMAT %%.%slf ->", pi_fmt);
-				sprintf(pi_fmt, "%%1.%dlf%s", i, "\n");
-				printf(pi_fmt, M_PI);
-		}
+    char pi_fmt[6] = {0};
+    for (int i = 0; i < 7; i += 2){
+	sprintf(pi_fmt, "%d", i);
+	printf("FORMAT %%.%slf ->", pi_fmt);
+	sprintf(pi_fmt, "%%1.%dlf%s", i, "\n");
+	printf(pi_fmt, M_PI);
+    }
 }
